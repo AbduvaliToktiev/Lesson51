a9fb4d88d51d318d12b4fa15dc1f98e75336175b
Admin <abdutokt2004>
2022-08-12 19:39:31 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Main {
+    public static void main(String[] args) {
+        Set<String> integers = new TreeSet<>();
+        integers.add("z");
+        integers.add("fsaf");
+        integers.add("sad");
+        integers.add("as");
+        integers.add("ab");
+        integers.add("a");
+        System.out.println(integers);
+    }
+}
